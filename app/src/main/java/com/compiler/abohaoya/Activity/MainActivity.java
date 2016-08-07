a911f852ed7c51509b4764a1612032ff42e1b690
SK MISHU <satyajitkaroo7@gmail.com>
2016-08-08 05:01:21 +0600

    Update copy from skm

--- app/src/main/java/com/compiler/abohaoya/Activity/MainActivity.java
+++ app/src/main/java/com/compiler/abohaoya/Activity/MainActivity.java
@@ -232,29 +232,6 @@ public class MainActivity extends AppCompatActivity {
         simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT+6"));
         String convertedTime = simpleDateFormat.format(new Date(millis * 1000));
 
-//
-//        Date date = new Date(millis);
-//        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
-//        String dateFormatted = formatter.format(date);
-
-
-//        long ts = millis;
-//        Date localTime = new Date(ts);
-//        String format = "yyyy/MM/dd HH:mm:ss";
-//        SimpleDateFormat sdf = new SimpleDateFormat(format);
-//
-//        // Convert Local Time to UTC (Works Fine)
-//        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-//        Date gmtTime = new Date(sdf.format(localTime));
-//        System.out.println("Local:" + localTime.toString() + "," + localTime.getTime() + " --> UTC time:"
-//                + gmtTime.toString() + "," + gmtTime.getTime());
-//
-//        // **** YOUR CODE **** END ****
-//
-//        // Convert UTC to Local Time
-//        Date fromGmt = new Date(gmtTime.getTime() + TimeZone.getDefault().getOffset(localTime.getTime()));
-//        System.out.println("UTC time:" + gmtTime.toString() + "," + gmtTime.getTime() + " --> Local:"
-//                + fromGmt.toString() + "-" + fromGmt.getTime());
         return convertedTime;
     }
 
